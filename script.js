1b9cc05d4e6b5d7499c2b5f09378909065eace4d
Isaak1234 <135656826+Isaak1234@users.noreply.github.com>
2025-08-05 14:38:08 -0300

    2

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textorincipal = document.querySelector(".caixa-principal");
+
+const perguntas = [
+    enunciado: "Pergunta 1", 
+    alternativas: [
+        "Alternativa 1", 
+        "Alternativa 2"]
+
+    "Pergunta 2"
+]
