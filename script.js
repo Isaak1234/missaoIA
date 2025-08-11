063e6237f02df08c297f0400c289d4bf8088475e
Isaak1234 <135656826+Isaak1234@users.noreply.github.com>
2025-08-11 12:00:09 -0300

    aaaaaaaaaaaaaa

--- script.js
+++ script.js
@@ -9,12 +9,42 @@ const perguntas = [
         enunciado: "Pergunta 1",
         alternativas: [
             "Alternativa 1",
-            "Alternativa 2"]
+            "Alternativa 2"
+        ]
     },
     {
         enunciado: "Pergunta 2",
         alternativas: [
             "Alternativa 1",
-            "Alternativa 2"]
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
     }
-]
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    
+}
